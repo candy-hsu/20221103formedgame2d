c8172168aa851430b1857a42c2e6b666bfbf48ca
candy-hsu <62374659+candy-hsu@users.noreply.github.com>
2022-11-03 15:39:37 +0800

    20221103-6

--- 20221103formedgame2d/Assets/Script/BgContrl.cs
+++ 20221103formedgame2d/Assets/Script/BgContrl.cs
@@ -1,7 +1,16 @@
-
+﻿
 using UnityEngine;
-
+namespace Uzai
+{
+    /// <summary>
+    /// 移動背景
+    /// </summary>
 public class BgContrl : MonoBehaviour
 {
-    
+
+        [SerializeField, Header("移動速度"), Range(-10, 0)]
+        private float speed = -3.5f;
 }
+
+}
+
---
+++ 20221103formedgame2d/Assets/Script/GrilController.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+namespace Uzai
+{
+    /// <summary>
+    /// 玩家控制器
+    /// </summary>
+    public class GrilController : MonoBehaviour
+    {
+        [Header("移動速度")]
+        [SerializeField, Range(0, 10)]
+        private float speedVertical = 3.5f;
+        [SerializeField, Range(0, 10)]
+        private float speedHorizontal = 4f;
+       
+        [Header("圖片")]
+        [SerializeField]
+        private Sprite stop;
+        [SerializeField]
+        private AnimationClip girlaim;
+    }
+}
+
